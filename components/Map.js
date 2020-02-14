7024babc68d4ab198eff2596503c50075eb8a5ee
Kayla Patel <paylakatel@gmail.com>
2020-02-14 11:41:02 -0500

    changes url for buffering back to our awsgeo url

--- components/Map.js
+++ components/Map.js
@@ -389,7 +389,7 @@ class Map extends React.Component {
         // the geojson we have to the necessary format.
         const esriJson = Util.geojsonToArcGIS(this.props.selectedParcel);
         request(
-          `https://utility.arcgisonline.com/ArcGIS/rest/services/Geometry/GeometryServer/buffer`,
+          `https://awsgeo.boston.gov/arcgis/rest/services/Utilities/Geometry/GeometryServer/buffer`,
           {
             geometries: {
               geometryType: 'esriGeometryPolygon',

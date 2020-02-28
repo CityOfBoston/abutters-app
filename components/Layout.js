5af760552c34b72eca50808e490562325b66d7bb
Kayla Patel <paylakatel@gmail.com>
2020-02-28 11:26:26 -0500

    addressing PR comments

--- components/Layout.js
+++ components/Layout.js
@@ -30,139 +30,15 @@ export default class Layout extends React.Component {
             rel="stylesheet"
             href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
           />
+          {/* When developing locally, this needs to be ./static/abutters-app.css. */}
+          <link rel="stylesheet" href="/abutters/static/abutters-app.css" />
           <meta
             name="viewport"
             content="width=device-width, initial-scale=1.0"
           />
         </Head>
-        {/* Make sure the heigh of the body element is the entire view port */}
-        <style global jsx>{`
-          // set fonts for elements
-          body,
-          h2,
-          h3,
-          h4,
-          h5,
-          label {
-            font-family: 'Montserrat';
-          }
-          p,
-          input {
-            font-family: 'Lora';
-            font-size: 18px;
-          }
-          p {
-            font-style: italic;
-            color: #616161;
-          }
-          // update link styles
-          a:hover {
-            text-decoration: none;
-            color: #fb4d42;
-          }
-          // pop-up styles
-          .dl-d {
-            clear: none;
-            width: 25%;
-          }
-          .dl-t {
-            clear: none;
-            width: 75%;
-          }
-          .dl-i {
-            padding: 0.2rem !important;
-          }
-          // select dropdown styles
-          .form-control {
-            border: 2px solid #091f2f;
-            font-family: 'Lora', serif;
-            font-style: italic;
-            border-radius: 0px;
-          }
-          .sel-c:after {
-            top: 2px;
-            bottom: 2px;
-            right: 2px;
-          }
-          @media only screen and (max-width: 500px) {
-            .mapboxgl-popup-tip {
-              visibility: hidden;
-            }
-            .mapboxgl-popup-content {
-              visibility: hidden;
-            }
-            #popupTable {
-              display: block;
-            }
-          }
-          // geocoder style
-          .mapboxgl-ctrl-geocoder input {
-            background-image: url(https://patterns.boston.gov/images/public/icons/search.svg);
-            background-repeat: no-repeat;
-            background-color: transparent;
-            background-position: right;
-            background-size: 20px 20px;
-            position: relative;
-            height: auto;
-            border: none;
-            border-bottom: 2px solid #091f2f;
-            border-radius: 0;
-            box-shadow: none;
-            font-family: 'Lora';
-            font-style: italic;
-            font-size: 20px;
-            min-width: 100%;
-          }
-          .mapboxgl-ctrl-geocoder--button {
-            display: none !important;
-          }
-          .mapboxgl-ctrl-geocoder--icon.mapboxgl-ctrl-geocoder--icon-loading {
-            display: none;
-          }
-          .mapboxgl-ctrl-geocoder--icon.mapboxgl-ctrl-geocoder--icon-search {
-            display: none;
-          }
-          .geocoder-icon {
-            visibility: hidden;
-          }
-          .suggestions {
-            font-family: Lora;
-            font-style: italic;
-            position: absolute;
-            z-index: 10000;
-            background-color: white;
-            border: 1px solid #091f2f;
-            padding: 0;
-            background-color: #f2f2f2;
-          }
-          .suggestions li {
-            list-style-type: none;
-            border-bottom: 1px solid #d2d2d2;
-            padding: 5px;
-          }
-          .suggestions li:hover {
-            background-color: #e0e0e0;
-          }
-          // button styles
-          .btn {
-            border-radius: 0px;
-          }
-          .btn-secondary {
-            background-color: #288be4;
-            border-color: #288be4;
-          }
-          .btn-secondary:focus {
-            box-shadow: 0 0 0 0.2rem rgba(40, 139, 228, 0.5);
-          }
-          .btn-secondary:not(:disabled):not(.disabled):active {
-            background-color: #288be4;
-            border-color: #288be4;
-          }
-          .btn:hover:not(:disabled) {
-            border-color: #fb4d42;
-          }
-        `}</style>
-        {/* set container div with room for navbar  */}
+        {/* Make sure the height of the body element is the entire view port and 
+        set container div with room for navbar  */}
         <div style={{ minHeight: 'calc(100vh - 125px)' }}>
           <Navbar>
             <div>
